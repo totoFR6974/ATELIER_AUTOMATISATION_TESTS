45f614c604cf4934b431ec66c3250d89e48d7ad1
toto_ <92277567+totoFR6974@users.noreply.github.com>
2026-03-11 16:45:15 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,44 +1,74 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+import requests
+import time
+from datetime import datetime
 
 app = Flask(__name__)
 
 
+db_runs = []
+run_counter = 1
+
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
+    return render_template('dashboard.html', runs=db_runs)
+
+
+@app.route('/run')
+def trigger_run():
+    global run_counter
+    
+
+    api_url = "https://api.agify.io?name=michael"
+    
+    start_time = time.time()
+    passed = 0
+    failed = 0
+    
+    try:
+     
+        response = requests.get(api_url, timeout=3)
+        latency = int((time.time() - start_time) * 1000)
+        
+
+        if response.status_code == 200:
+            passed += 1
+        else:
+            failed += 1
+            
+       
+        try:
+            data = response.json()
+            passed += 1
+        except ValueError:
+            failed += 1
+            
+        status = "success" if failed == 0 else "warning"
+        
+    except requests.exceptions.RequestException:
+        
+        latency = int((time.time() - start_time) * 1000)
+        failed = 2
+        status = "danger"
+        
+
+    new_run = {
+        "id": run_counter,
+        "timestamp": datetime.now().strftime("%d %b %Y - %H:%M:%S"),
+        "api": "Agify",
+        "passed": passed,
+        "failed": failed,
+        "latency_avg_ms": latency,
+        "status": status
+    }
+    
+
+    db_runs.insert(0, new_run)
+    run_counter += 1
+    
    
-    
-    mock_runs = [
-        {
-            "id": 3,
-            "timestamp": "11 Mars 2026 - 15:25",
-            "api": "Agify",
-            "passed": 6,
-            "failed": 0,
-            "latency_avg_ms": 145,
-            "status": "success"
-        },
-        {
-            "id": 2,
-            "timestamp": "11 Mars 2026 - 15:20",
-            "api": "Agify",
-            "passed": 5,
-            "failed": 1,
-            "latency_avg_ms": 520,
-            "status": "warning"
-        },
-        {
-            "id": 1,
-            "timestamp": "11 Mars 2026 - 15:15",
-            "api": "Agify",
-            "passed": 6,
-            "failed": 0,
-            "latency_avg_ms": 130,
-            "status": "success"
-        }
-    ]
-    
-    return render_template('dashboard.html', runs=mock_runs)
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.run(debug=True)
